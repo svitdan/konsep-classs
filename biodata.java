30ad6259456d50de76b04f42bfe530299b54c2f9
svitdan <75371271+svitdan@users.noreply.github.com>
2021-02-25 15:54:53 +0700

    Add files via upload

---
+++ biodata.java
@@ -0,0 +1,18 @@
+public class biodata_diri {
+   public static void main (String [] args) {
+       //deklarasi variable
+       String nama,alamat,hobi,sekolah,umur;
+
+       //pengisian variable
+       nama = "CHIKA YUSTANTI";
+       alamat = "PURWODADI PASURUAN";
+       hobi = "SHOOPING";
+       sekolah = "SMKN 1 PURWOSARI";
+
+       //tampil
+       System.out.println("NAMA : " + nama);
+       System.out.println("ALAMAT : " + alamat);
+       System.out.println("HOBI : " + hobi);
+       System.out.println("SEKOLAH : " + sekolah);
+   }
+}
